e73f4a00975fd6aa89567a2f9ac287c75ff325f5
aly ba <alybas2002@yahoo.fr>
2013-12-30 11:18:21 +0100

    application boursière partie 1

---
+++ C#-interfaces/concrete-vs-interface/PeopleViewer/MainWindow.xaml.cs
@@ -0,0 +1,53 @@
+﻿using PersonLibrary;
+using System.Collections;
+using System.Collections.Generic;
+using System.Windows;
+
+namespace PeopleViewer
+{
+    public partial class MainWindow : Window
+    {
+        PeopleRepository peopleRepo = new PeopleRepository();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            
+        }
+
+        private void ConcreteFetchButton_Click(object sender, RoutedEventArgs e)
+        {
+            ClearListBox();
+            //tableau de person; liste enumerable 
+            List  <Person> people;
+            people = peopleRepo.GetPeople();
+            foreach (var person in people)
+            {
+                PersonListBox.Items.Add(person);
+            }
+           
+        }
+
+        private void InterfaceFetchButton_Click(object sender, RoutedEventArgs e)
+        {
+            ClearListBox();
+            //type enumerable
+            IEnumerable people;
+            people = peopleRepo.GetPeople();
+            foreach (var person in people)
+            {
+                PersonListBox.Items.Add(person);
+            }
+        }
+
+        private void ClearButton_Click(object sender, RoutedEventArgs e)
+        {
+            ClearListBox();
+        }
+
+        private void ClearListBox()
+        {
+            PersonListBox.Items.Clear();
+        }
+    }
+}
